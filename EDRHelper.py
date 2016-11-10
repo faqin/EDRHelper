248b5cf40163a3a0b4cddf113c045aa654b5ba4e
faqin <woddythink@gmail.com>
2016-11-10 21:52:25 +0800

    update and fix

--- EDRHelper.py
+++ EDRHelper.py
@@ -1,21 +1,42 @@
 # coding=utf-8
-import idc
-import idaapi
+
+from idaapi import *
+from idc import *
+from idautils import *
 
 class EDRHelper:
 
-    
-    def get_func_called_addr(self,func_name=None):
 
-        return func_name and [ func_called_addr for func_called_addr in CodeRefsTo(LocByName(func_name),0) ] or []
+    def __init__(self,*tuple_func):
+        self.tuple_func=tuple_func
+        self.bp()
+
+
+     
     
-        
+    def bp(self):
+        func_list=self.get_func_called_addr(self.tuple_func)
+
+        for addr in func_list:
+            AddBpt(addr)
+
+    def get_func_called_addr(self,func_name):
+        l=[]
+
+        gen_addr=[CodeRefsTo(func_addr,0) for func_addr in (LocByName(func) for func in func_name)]
+
+        for refs in gen_addr:
+            for ref in refs:
+                l.append(ref)
+        return l
+                
 
     def get_arg_value(self,arg_name):
         regvalue=idc.GetRegValue(arg_name)
         if self.is_addr(regvalue):
             return self.get_asc_str(regvalue)
         else:
+            
             return regvalue
 
 
@@ -32,10 +53,7 @@ class EDRHelper:
             asc_str+=chr(idc.Byte(addr))
             addr+=1
         return asc_str
-    
-    
 
 
 
 
-        
--- EDRHooks.py
+++ EDRHooks.py
@@ -1,28 +1,32 @@
 # coding=utf-8
+
 from idaapi import *
-import idc
+from idc import *
 from EDRHelper import *
+from idautils import *
+
 class EDRHooks(DBG_Hooks):
     
+    def user_helper(self):
 
+        self.helper=EDRHelper("nvram_bufget","nvram_get")
+        self.file_handle=open("result.txt","a+")
+        
     def dbg_bpt(self,tid,ea):
 
         print "hit on 0x%08x" % ea
        
-        print type(ea)
+        
         
         self.dbgstep_into(ea)
         
 
-        helper=EDRHelper()
-        
-
-        print helper.get_arg_value("A1")
-       
-            
+        key=self.helper.get_arg_value("A1")
+        self.file_handle.write(key+"\n")
+        self.file_handle.flush()
 
         idaapi.continue_process()
-        #print  self.get_arg_value("A1")
+        
         
 
     def dbgstep_into(self,ea):
@@ -34,6 +38,7 @@ class EDRHooks(DBG_Hooks):
         #必须用GetDebuggerEvent处理step_into()
         GetDebuggerEvent(WFNE_SUSP , -1)
         EnableBpt(ea,1)
+
         
 
 
@@ -41,8 +46,10 @@ class EDRHooks(DBG_Hooks):
 
 debugger=EDRHooks()
 debugger.hook()
-
+debugger.user_helper()
+'''
 AddBpt(0x45fdb4)
 AddBpt(0x45fEF0)
 AddBpt(0x45ff78)
 AddBpt(0x4600A0)
+'''
